9773448eadbf62746ec341e4bfb4f98bb41395a9
Waysys <wshaffer@waysysweb.com>
2024-06-28 13:16:40 -0400

    Convert Holiday code to ES6

--- datebench/model/Holiday.js
+++ datebench/model/Holiday.js
@@ -1,67 +1,84 @@
 /**
- * Copyright (c) 2014 Waysys LLC
+ * Copyright (c) 2024 Waysys LLC
  *
  * Date: 5/25/14
- * Time: 3:16 PM
  *
  * Date        Author     Descriptions
  * ----------- ---------- ----------------------------------------------------
  * 05-25-2014  W Shaffer  File created
  * 12-15-2016  W Shaffer  Updated to comply with ESLint standard rule
  * 12-24-2016  W Shaffer  Turned globals into class properties
+ * 06-27-2024  W Shaffer  Converted to ES6 syntax
  */
+
+import { WayDate } from "./WayDate.js";
+export { Holiday }
+
+/**
+ * Enumerated values of the position in the month
+ */
+Holiday.Position = {
+    FIRST: 1,
+    SECOND: 2,
+    THIRD: 3,
+    FOURTH: 4,
+    LAST: 5
+}
 
 /**
  * Holiday is a class for creating the dates of United States holidays
  */
-function Holiday() {
-    "use strict";
-}
-
-Holiday.prototype = {
+class Holiday {
 
     /**
      * Return the date for New Year's Day.
      *
-     * @param year the desired year
+     * @param {number} year the desired year
      * @return (WayDate) the date of New Year's Day for the specified year
      */
-    newYearsDay: function (year) {
-        "use strict";
+    static newYearsDay(year) {
         return new WayDate(1, 1, year);
-    },
+    }
 
     /**
      * Return the date for Martin Luther King
      *
-     * @param year the desired year
-     * @return the date of Marting Luther King's
+     * @param {number} year the desired year
+     * @return (WayDate) the date of Marting Luther King's
      */
-    martinLutherKingsBirthday: function (year) {
-        "use strict";
+    static martinLutherKingsBirthday(year) {
         return this.dateFromPosition(1, year, 1, Holiday.Position.THIRD);
-    },
+    }
 
     /**
      * Return the date of Washington's Birthday
      *
      * @param {number} year the year for the date
+     * @return (WayDate) the date of Washingtons Birthday
      */
-    washingtonsBirthday: function (year) {
-        "use strict";
+    static washingtonsBirthday(year) {
         return this.dateFromPosition(2, year, 1, Holiday.Position.THIRD);
-    },
+    }
 
     /**
      * Return the date of Memorial Day for the year
      *
      * @param {number} year the year of the date
-     * @return the date of memorial day
+     * @return (WayDate) the date of memorial day
      */
-    memorialDay: function (year) {
-        "use strict";
+    static memorialDay(year) {
         return this.dateFromPosition(5, year, 1, Holiday.Position.LAST);
-    },
+    }
+
+    /**
+     * Return the date of Juneteenth
+     * 
+     * @param {number} year the year of the date
+     * @returns the date of Juneteenth
+     */
+    static juneteenth(year) {
+        return new WayDate(6, 19, year)
+    }
 
     /**
      * Return the date of the Independence Day
@@ -69,10 +86,9 @@ Holiday.prototype = {
      * @param {number} year the year of the date
      * @returns (WayDate) the date of Independence Day
      */
-    independenceDay: function (year) {
-        "use strict";
+    static independenceDay(year) {
         return new WayDate(7, 4, year);
-    },
+    }
 
     /**
      * Return the date of Labor Day.
@@ -80,10 +96,9 @@ Holiday.prototype = {
      * @param {number} year year of the date
      * @returns the date of Labor Day
      */
-    laborDay: function (year) {
-        "use strict";
+    static laborDay(year) {
         return this.dateFromPosition(9, year, 1, Holiday.Position.FIRST);
-    },
+    }
 
     /**
      * Return the date of Columbus Day.
@@ -91,53 +106,48 @@ Holiday.prototype = {
      * @param {number} year the year of the date
      * @returns the date of Columbus Day
      */
-    columbusDay: function (year) {
-        "use strict";
+    static columbusDay(year) {
         return this.dateFromPosition(10, year, 1, Holiday.Position.SECOND);
-    },
+    }
 
     /**
      * Return the date of Veterans Day.
      *
      * @param {number} year the year of the date
-     * @return {WayDate} the date of Veterans Day
+     * @returns {WayDate} the date of Veterans Day
      */
-    veteransDay: function (year) {
-        "use strict";
+    static veteransDay(year) {
         return new WayDate(11, 11, year);
-    },
+    }
 
     /**
      * Return the date of Thanksgiving.
      *
      * @param {number} year the year of the date
-     * @return {WayDate} the date of Thanksgiving
+     * @returns {WayDate} the date of Thanksgiving
      */
-    thanksgiving: function (year) {
-        "use strict";
+    static thanksgiving(year) {
         return this.dateFromPosition(11, year, 4, Holiday.Position.FOURTH);
-    },
+    }
 
     /**
      * Return the date of Christmas.
      *
      * @param {number} year the year of the date
-     * @return {WayDate} the date of Christmas
+     * @returns {WayDate} the date of Christmas
      */
-    christmas: function (year) {
-        "use strict";
+    static christmas(year) {
         return new WayDate(12, 25, year);
-    },
+    }
 
     /**
      * Return the date the holiday is observed at the specified index.
      *
      * @param index an index from 0 through 9
      * @param year the desired year of the holiday
-     * @return a observed holiday date
+     * @returns a observed holiday date
      */
-    observedHolidays: function (index, year) {
-        "use strict";
+    static observedHolidays(index, year) {
         var date;
         this.assert(!isNaN(year), "holiday: year must be a number");
         this.assert(year >= 1900,
@@ -180,74 +190,63 @@ Holiday.prototype = {
             throw new Error("holiday: invalid index: " + index);
         }
         return date;
-    },
+    }
 
     /**
      * Return the date of the holiday at the specified index.
      *
      * @param index an index from 0 through 9
      * @param year the desired year of the holiday
-     * @return a holiday date
+     * @returns a holiday date
      */
-    holidays: function (index, year) {
-        "use strict";
-        var date;
+    static holidays(index, year) {
+        let date;
         this.assert(!isNaN(year), "holiday: year must be a number");
         this.assert(year >= 1900,
             "holiday: holiday year must be equal or greater than 1900: " + year);
+
         switch (index) {
         case 0:
-            date = this.newYearsDay(year);
+            date = WayDate.newYearsDay(year);
             break;
         case 1:
-            date = this.martinLutherKingsBirthday(year);
+            date = WayDate.martinLutherKingsBirthday(year);
             break;
         case 2:
-            date = this.washingtonsBirthday(year);
+            date = WayDate.washingtonsBirthday(year);
             break;
         case 3:
-            date = this.easter(year);
+            date = WayDate.easter(year);
             break;
         case 4:
-            date = this.memorialDay(year);
+            date = WayDate.memorialDay(year);
             break;
         case 5:
-            date = this.independenceDay(year);
+            date = WayDate.juneteenth(year);
             break;
         case 6:
-            date = this.laborDay(year);
+            date = WayDate.independenceDay(year);
             break;
         case 7:
-            date = this.columbusDay(year);
+            date = WayDate.laborDay(year);
             break;
         case 8:
-            date = this.veteransDay(year);
+            date = WayDate.columbusDay(year);
             break;
         case 9:
-            date = this.thanksgiving(year);
+            date = this.veteransDay(year);
             break;
         case 10:
+            date = this.thanksgiving(year);
+            break;
+        case 11:
             date = this.christmas(year);
             break;
         default:
             throw new Error("holiday: invalid index: " + index);
         }
         return date;
-    },
-
-    names: [
-        "New Year's Day",
-        "Birthday of Martin Luther King, Jr.",
-        "Washington's Birthday",
-        "Easter",
-        "Memorial Day",
-        "Independence Day",
-        "Labor Day",
-        "Columbus Day",
-        "Veterans Day",
-        "Thanksgiving Day",
-        "Christmas"
-    ],
+    }
 
     /**
      * Return the name of the holiday for a given index.
@@ -255,7 +254,22 @@ Holiday.prototype = {
      * @param {number} index (0 <= index <= 10}
      * @returns {string} the name of the holiday
      */
-    getName: function (index) {
+    static getName(index) {
+        const names = [
+            "New Year's Day",
+            "Birthday of Martin Luther King, Jr.",
+            "Washington's Birthday",
+            "Easter",
+            "Memorial Day",
+            "Juneteenth",
+            "Independence Day",
+            "Labor Day",
+            "Columbus Day",
+            "Veterans Day",
+            "Thanksgiving Day",
+            "Christmas"
+        ];
+
         if (isNaN(index)) {
             throw new Error("getName: invalid index: " + index);
         } else if (index < 0) {
@@ -263,8 +277,8 @@ Holiday.prototype = {
         } else if (index >= this.names.length) {
             throw new Error("getName: invalid index: " + index);
         }
-        return this.names[index];
-    },
+        return names[index];
+    }
 
 
     /* -------------------------------------------------------------------------
@@ -277,12 +291,11 @@ Holiday.prototype = {
      * @param {Boolean} result the result of a test
      * @param {String} descr the description of the test
      */
-    assert: function (result, descr) {
-        "use strict";
+    static assert(result, descr) {
         if (!result) {
             throw new Error(descr);
         }
-    },
+    }
 
     /**
      * Return a date with the specified month and year and day of week. The
@@ -296,15 +309,14 @@ Holiday.prototype = {
      *            the day of the week (dayOfWeek inset range(SUNDAY, SATURDAY))
      * @param {number} monthPosition
      *            the month position (monthPosition inset range(LAST, FOURTH))
-     * @return (WayDate) an instance of WayDate with the specified month, year, day of
+     * @returns (WayDate) an instance of WayDate with the specified month, year, day of
      *         week and month position
      */
-    dateFromPosition: function (month, year, dayOfWeek, monthPosition) {
-        "use strict";
-        var date;
+    static dateFromPosition(month, year, dayOfWeek, monthPosition) {
         /*
          Create initial date
          */
+        let date;
         if (monthPosition === Holiday.Position.LAST) {
             date = new WayDate(month, WayDate.daysInMonth(month, year), year);
         } else {
@@ -322,7 +334,7 @@ Holiday.prototype = {
          *
          */
         return date;
-    },
+    }
 
     /**
      * Return the nth day that is on a specified day of the week from a
@@ -334,10 +346,9 @@ Holiday.prototype = {
      *            the day of the week (dayOfWeek inset range(0, 6))
      * @param {number} monthPosition
      *            the position in the month
-     * @return a date in the Nth position
+     * @returns a date in the Nth position
      */
-    nthDayFromDate: function (inputDate, dayOfWeek, monthPosition) {
-        "use strict";
+    static nthDayFromDate(inputDate, dayOfWeek, monthPosition) {
         var offset, date;
         /*
          Get working date
@@ -353,17 +364,16 @@ Holiday.prototype = {
          position of date in month = monthPosition
          */
         return date;
-    },
+    }
 
     /**
      * Return the offset used to correct a date
      *
      * @param {number} monthPosition
      *          the position in the month
-     * @return (number) the offset used to correct the date
+     * @returns (number) the offset used to correct the date
      */
-    calculateOffset: function (monthPosition) {
-        "use strict";
+    static calculateOffset(monthPosition) {
         var offset = 0;
         switch (monthPosition) {
         case Holiday.Position.FIRST:
@@ -386,7 +396,7 @@ Holiday.prototype = {
                 monthPosition);
         }
         return offset;
-    },
+    }
 
     /**
      * Return a date on a specified day of the week on or before a specified
@@ -399,10 +409,7 @@ Holiday.prototype = {
      * @returns the date such that date.dayOfWeekNumber() = dayOfWeek and
      *         date.compare(inputDate) <> GREATER
      */
-    dateOnDayOfWeekOnOrBefore: function (inputDate, dayOfWeek) {
-        "use strict";
-        var date1, priorDayOfWeek, date;
-
+    static dateOnDayOfWeekOnOrBefore(inputDate, dayOfWeek) {
         this.assert(inputDate.valid(),
             "dateOnDayOfWeekOnOrBefore: invalid input date");
         this.assert(dayOfWeek >= 0,
@@ -410,9 +417,9 @@ Holiday.prototype = {
         this.assert(dayOfWeek < 7,
             "dateOnDayOfWeekOnOrBefore: invalid dayOfWeek - " + dayOfWeek);
 
-        date1 = inputDate.add(-dayOfWeek);
-        priorDayOfWeek = date1.dayOfWeekNumber();
-        date = inputDate.add(-priorDayOfWeek);
+        const date1 = inputDate.add(-dayOfWeek);
+        const priorDayOfWeek = date1.dayOfWeekNumber();
+        const date = inputDate.add(-priorDayOfWeek);
 
         this.assert(date.dayOfWeekNumber() === dayOfWeek,
             "dateOnDayOfWeekOnOrBefore: incorrect day of week" +
@@ -420,7 +427,7 @@ Holiday.prototype = {
         this.assert(date.compare(inputDate) !== WayDate.Compare.GREATER,
             "dateOnDayOfWeekOnOrBefore: date is greater than input");
         return date;
-    },
+    }
 
     /**
      * Return the date on a specified day of the week after a specified date
@@ -429,16 +436,14 @@ Holiday.prototype = {
      *            the date being considered
      * @param dayOfWeek
      *            the day of the week (0 <= dayOfWeek < 7)
-     * @return the date such that dayOfWeek(date) = dayOfWeek and
+     * @returns the date such that dayOfWeek(date) = dayOfWeek and
      *         date.compare(inputDate) = GREATER
      */
-    dateOnDayOfWeekAfter: function (inputDate, dayOfWeek) {
-        "use strict";
-        var date;
-        date = inputDate.add(7);
+    static dateOnDayOfWeekAfter(inputDate, dayOfWeek) {
+        let date = inputDate.add(7);
         date = this.dateOnDayOfWeekOnOrBefore(date, dayOfWeek);
         return date;
-    },
+    }
 
     /**
      * Return the date on a specified day of the week before a specified date
@@ -447,18 +452,16 @@ Holiday.prototype = {
      *            the date being considered
      * @param dayOfWeek
      *            the day of the week (dayOfWeek inset range(0, 6))
-     * @return the date such that dayOfWeek(date) = dayOfWeek andalso
+     * @returns the date such that dayOfWeek(date) = dayOfWeek andalso
      *         compare(inputDate, date) = GREATER
      * @exception WayDateException
      *                thrown if the result of adds are out of bounds
      */
-    dateOnDayOfWeekBefore: function (inputDate, dayOfWeek) {
-        "use strict";
-        var date;
-        date = inputDate.add(-1);
+    static dateOnDayOfWeekBefore(inputDate, dayOfWeek) {
+        let date = inputDate.add(-1);
         date = this.dateOnDayOfWeekOnOrBefore(date, dayOfWeek);
         return date;
-    },
+    }
 
     /**
      * Return the date a holiday would be observed if it were a federal holiday.
@@ -467,9 +470,8 @@ Holiday.prototype = {
      *            the holiday being checked
      * @returns the date a holiday will be observed
      */
-    observedHoliday: function (date) {
-        "use strict";
-        var dayOfWeek = date.dayOfWeekNumber();
+    static observedHoliday(date) {
+        let dayOfWeek = date.dayOfWeekNumber();
         switch (dayOfWeek) {
         case 0: // Sunday
             date = date.add(1);
@@ -479,7 +481,7 @@ Holiday.prototype = {
             break;
         }
         return date;
-    },
+    }
 
     /* -------------------------------------------------------------------------
      Easter
@@ -489,18 +491,16 @@ Holiday.prototype = {
      * Return the date of Easter
      *
      * @param year the desired year
-     * @return the date of easter
+     * @returns the date of easter
      */
-    easter: function (year) {
-        "use strict";
-        var date, datePaschalMoon;
+    static easter(year) {
         this.assert(!isNaN(year), "easter: year must be a number - " + year);
         this.assert(year >= 1900, "easter: invalid year = " + year);
 
-        datePaschalMoon = this.paschalMoon(year);
-        date = this.dateOnDayOfWeekAfter(datePaschalMoon, 0);
+        const datePaschalMoon = this.paschalMoon(year);
+        const date = this.dateOnDayOfWeekAfter(datePaschalMoon, 0);
         return date;
-    },
+    }
 
     /**
      * Return the date of the paschal moon
@@ -508,24 +508,23 @@ Holiday.prototype = {
      * @param year (number) the year of the date
      * @returns (WayDate) the date of the paschal moon for the year
      */
-    paschalMoon: function (year) {
-        "use strict";
-        var date, adjustment;
-        date = new WayDate(4, 19, year);
-        adjustment = this.adjustedEpach(year);
+    static paschalMoon(year) {
+        let date = new WayDate(4, 19, year);
+        const adjustment = this.adjustedEpach(year);
         date = date.add(-adjustment);
         return date;
-    },
+    }
 
     /**
      * Return the adjusted epach
      *
      * @param year (number) the year of interest
+     * @returns the adjusted epach
      */
-    adjustedEpach: function (year) {
-        "use strict";
-        var shifted, result;
-        shifted = this.shiftedEpach(year);
+    static adjustedEpach(year) {
+        let result;
+        const shifted = this.shiftedEpach(year);
+
         if ((shifted === 0) ||
             ((shifted === 1) && ((year % 19) > 10))
         ) {
@@ -534,33 +533,23 @@ Holiday.prototype = {
             result = shifted;
         }
         return result;
-    },
+    }
 
     /**
      * Return the shifted epach
      *
      * @param year the year of interest
-     * @return the shifted epach
+     * @returns the shifted epach
      */
-    shiftedEpach: function (year) {
-        "use strict";
-        var cent, result;
-        cent = Math.floor(year / 100);
-        result =
+    static shiftedEpach(year) {
+        const cent = Math.floor(year / 100);
+        const result =
             (14 + (11 * (year % 19)) - Math.floor((3 * cent) / 4) +
             Math.floor((5 + 8 * cent) / 25)) % 30;
         return result;
     }
 
-};
+}
 
-/**
- * Enumerated values of the position in the month
- */
-Holiday.Position = {
-    FIRST: 1,
-    SECOND: 2,
-    THIRD: 3,
-    FOURTH: 4,
-    LAST: 5
-};
+
+
